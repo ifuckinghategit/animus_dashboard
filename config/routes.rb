e73f0c615495de37cd82d07f6565ade97ea7623b
Ben Clarke <benclarke018@gmail.com>
2015-07-22 15:01:08 +0100

    Removed main nav (kept files) and maked the sign in page as root

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
 
   get 'dashboard/index' => 'dashboard#index'
 
   devise_for :users
+
+  devise_scope :user do
+    root :to => 'devise/sessions#new'
+  end
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
